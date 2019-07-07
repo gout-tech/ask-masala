e1708d16238c3c4e052c5fbe8eed7d1605b6e89d
gouthz <goutham.hashroot@gmail.com>
2019-07-07 10:07:25 +0530

    Logging

--- askmasala/apps/about_us/views.py
+++ askmasala/apps/about_us/views.py
@@ -1,3 +1,5 @@
+import logging
+log = logging.getLogger(__name__)
 from django.views.generic import ListView
 from apps.about_us.models import About
 
@@ -9,4 +11,9 @@ class AboutUs(ListView):
 
     def get_queryset(self):
 
-        return About.objects.filter(is_published=True)
+       return About.objects.filter(is_published=True)
+
+log.debug("Hey there it works!!")
+log.info("Hey there it works!!")
+log.warn("Hey there it works!!")
+log.error("Hey there it works!!")
--- askmasala/askmasala/settings/base.py
+++ askmasala/askmasala/settings/base.py
@@ -85,9 +85,9 @@ WSGI_APPLICATION = 'askmasala.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'askmasla2',
-        'USER': 'username',
-        'PASSWORD': 'mypassword',
+        'NAME': 'askmasala',
+        'USER': 'user',
+        'PASSWORD': '',
     }
 }
 
@@ -136,3 +136,58 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 #STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'staticfiles'), )
 
+import logging.config
+import os
+from django.utils.log import DEFAULT_LOGGING
+
+# Disable Django's logging setup
+LOGGING_CONFIG = None
+
+LOGLEVEL = os.environ.get('LOGLEVEL', 'info').upper()
+
+logging.config.dictConfig({
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'default': {
+            # exact format is not important, this is the minimum information
+            'format': '%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+        },
+        'django.server': DEFAULT_LOGGING['formatters']['django.server'],
+    },
+    'handlers': {
+        # console logs to stderr
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'default',
+        },
+        # Add Handler for Sentry for `warning` and above
+        'sentry': {
+            'level': 'WARNING',
+            'class': 'raven.contrib.django.raven_compat.handlers.SentryHandler',
+        },
+        'django.server': DEFAULT_LOGGING['handlers']['django.server'],
+    },
+    'loggers': {
+        # default for all undefined Python modules
+        '': {
+            'level': 'WARNING',
+            'handlers': ['console', 'sentry'],
+        },
+        # Our application code
+        'app': {
+            'level': LOGLEVEL,
+            'handlers': ['console', 'sentry'],
+            # Avoid double logging because of root logger
+            'propagate': False,
+        },
+        # Prevent noisy modules from logging to Sentry
+        'noisy_module': {
+            'level': 'ERROR',
+            'handlers': ['console'],
+            'propagate': False,
+        },
+        # Default runserver request logging
+        'django.server': DEFAULT_LOGGING['loggers']['django.server'],
+    },
+})
